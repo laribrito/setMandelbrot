8ccf8bb9828c0558de5d5afbf485ec7f60fca2e9
laribrito <lary.29.ds@gmail.com>
2026-09-23 11:12:06 -0300

    build: add paralelo_collapse to compilation script

--- compile.py
+++ compile.py
@@ -6,7 +6,8 @@ def main():
     command1 = "g++ -O3 -ffast-math -o sequencial sequencial.cpp"
     command2 = "g++ -O3 -ffast-math -std=c++17 -o ponto_a_ponto ponto_a_ponto.cpp"
     command3 = "g++ -O3 -ffast-math -fopenmp -std=c++17 -o paralelo paralelo.cpp"
-    command4 = "g++ -O3 -o comparador comparador.cpp"
+    command4 = "g++ -O3 -ffast-math -fopenmp -std=c++17 -o paralelo_collapse paralelo_collapse.cpp"
+    command5 = "g++ -O3 -o comparador comparador.cpp"
     
     # Adicionando o mingw64 ao PATH dinamicamente apenas no Windows para o g++ encontrar suas DLLs
     env = os.environ.copy()
@@ -22,6 +23,8 @@ def main():
         subprocess.run(command3, check=True, shell=True, env=env)
         print(f"Executando: {command4}")
         subprocess.run(command4, check=True, shell=True, env=env)
+        print(f"Executando: {command5}")
+        subprocess.run(command5, check=True, shell=True, env=env)
         print("Compilação de todas as versões concluída com sucesso.")
     except subprocess.CalledProcessError as e:
         print(f"Erro durante a compilação: {e}")
