7fbedf7049d1fa461992eeb6fb22b56c726c6bd6
laribrito <lary.29.ds@gmail.com>
2026-09-22 22:42:15 -0300

    feat: implementa versao paralela com OpenMP e suporte a escalonamento configuravel

--- compile.py
+++ compile.py
@@ -4,8 +4,9 @@ import sys
 
 def main():
     command1 = "g++ -O3 -ffast-math -o sequencial sequencial.cpp"
-    command2 = "g++ -O3 -ffast-math -o ponto_a_ponto ponto_a_ponto.cpp"
-    command3 = "g++ -O3 -o comparador comparador.cpp"
+    command2 = "g++ -O3 -ffast-math -std=c++17 -o ponto_a_ponto ponto_a_ponto.cpp"
+    command3 = "g++ -O3 -ffast-math -fopenmp -std=c++17 -o paralelo paralelo.cpp"
+    command4 = "g++ -O3 -o comparador comparador.cpp"
     
     # Adicionando o mingw64 ao PATH dinamicamente apenas no Windows para o g++ encontrar suas DLLs
     env = os.environ.copy()
@@ -19,6 +20,8 @@ def main():
         subprocess.run(command2, check=True, shell=True, env=env)
         print(f"Executando: {command3}")
         subprocess.run(command3, check=True, shell=True, env=env)
+        print(f"Executando: {command4}")
+        subprocess.run(command4, check=True, shell=True, env=env)
         print("Compilação de todas as versões concluída com sucesso.")
     except subprocess.CalledProcessError as e:
         print(f"Erro durante a compilação: {e}")
